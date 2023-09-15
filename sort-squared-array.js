88b590f4244e908b74bfea246821d8d039ccd479
morahgeist <114309093+morahgeist@users.noreply.github.com>
2023-09-15 13:04:50 -0700

    sort squared arr solution

---
+++ sort-squared-array.js
@@ -0,0 +1,9 @@
+/*
+Write a function that takes in a non-empty array of integers that are sorted in ascending order and returns a new array of the same length with the squares of the original integers also sorted in ascending order.
+
+array = [1, 2, 3, 5, 6, 8, 9] -> [1, 4, 9, 25, 36, 64, 81]
+*/
+
+function sortedSquaredArray(array) {
+  return array.map((el) => el ** 2).sort((a, b) => a - b);
+}
