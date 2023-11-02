1b23f22dfa1d03780a4628071d24c99420db75d0
morahgeist <114309093+morahgeist@users.noreply.github.com>
2023-11-02 10:57:39 -0700

    is prime working

---
+++ isPrime.js
@@ -0,0 +1,22 @@
+const isPrime = (n) => {
+  if (n < 2) return false;
+  if (n === 2) return true;
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) return false;
+  }
+  return true;
+};
+
+console.log(isPrime(2)); // -> true
+console.log(isPrime(3)); // -> true
+console.log(isPrime(4)); // -> false
+console.log(isPrime(5)); // -> true
+console.log(isPrime(6)); // -> false
+console.log(isPrime(7)); // -> true
+console.log(isPrime(8)); // -> false
+console.log(isPrime(25)); // -> false
+console.log(isPrime(31)); // -> true
+console.log(isPrime(2017)); // -> true
+console.log(isPrime(2048)); // -> false
+console.log(isPrime(1)); // -> false
+console.log(isPrime(713)); // -> false
